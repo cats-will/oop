c7d59745332b7452685bca363685b62c3cfc04d0
cats-will <leika181@rambler.ru>
2020-12-01 00:28:11 +0300

    commit

---
+++ oop4/cmake-build-debug/CMakeFiles/oop4.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/catswill/oop/oop4/Backup.cpp" "/home/catswill/oop/oop4/cmake-build-debug/CMakeFiles/oop4.dir/Backup.cpp.o"
+  "/home/catswill/oop/oop4/RestorePoint.cpp" "/home/catswill/oop/oop4/cmake-build-debug/CMakeFiles/oop4.dir/RestorePoint.cpp.o"
+  "/home/catswill/oop/oop4/StorageAlgorithm.cpp" "/home/catswill/oop/oop4/cmake-build-debug/CMakeFiles/oop4.dir/StorageAlgorithm.cpp.o"
+  "/home/catswill/oop/oop4/main.cpp" "/home/catswill/oop/oop4/cmake-build-debug/CMakeFiles/oop4.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ oop4/cmake-build-debug/CMakeFiles/oop4.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oop4.dir/Backup.cpp.o"
+  "CMakeFiles/oop4.dir/RestorePoint.cpp.o"
+  "CMakeFiles/oop4.dir/StorageAlgorithm.cpp.o"
+  "CMakeFiles/oop4.dir/main.cpp.o"
+  "oop4"
+  "oop4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oop4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
